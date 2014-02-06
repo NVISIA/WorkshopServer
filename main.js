386e8ba8970833976dbeb19e461f2de2cf4271c9
Jeff Gitter <gitter1226@gmail.com>
2014-02-06 16:23:29 -0600

    Typo

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ var restaurantServiceRoutes = {
     'get': {
         '/restaurants': [security.secureService, restaurantService.getRestaurants],
         '/restaurants/:id': [security.secureService, restaurantService.getRestaurant],
-        '/restaurants/:id/reservations': [security.secureService, restaurantService,getReservations]
+        '/restaurants/:id/reservations': [security.secureService, restaurantService.getReservations]
     },
     'put': {
         '/restaurants': [security.secureService, restaurantService.saveRestaurant],
